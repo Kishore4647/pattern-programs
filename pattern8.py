9a4dbfe7594692fdb936e18238a7381190cf1ac1
Kishore4647 <155071422+Kishore4647@users.noreply.github.com>
2024-12-13 13:00:27 +0530

    Add files via upload

--- pattern8.py
+++ pattern8.py
@@ -1,18 +1,17 @@
-n=int(input())
+n=5
 stars=1
-spaces=(n*2)-2
+space=n-1
 for row in range(1,n+1):
     dum=1
-    dum2=row
-    for col in range(1,stars+1):
-        print(dum,end=' ')
-        dum+=1
-    for sp in range(1,spaces+1):
-        print(' ',end=' ')
+    for sp in range(1,space+1):
+        print(' ',end='')
     for st in range(1,stars+1):
-        print(dum2,end=' ')
-        dum2-=1
+        print(dum,end='')
+        if st>=row:
+            dum-=1
+        else:
+            dum+=1
+            
     print()
-    stars+=1
-    spaces-=2
-    
+    stars+=2
+    space-=1
